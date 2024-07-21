860761187df41722d9645d49810567275108a493
Sabricetin <sabriyazilim@gmail.com>
2024-07-21 23:19:01 +0300

    Initial Commit

---
+++ Angry-Birds-CloneUITests/Angry_Birds_CloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Angry_Birds_CloneUITestsLaunchTests.swift
+//  Angry-Birds-CloneUITests
+//
+//  Created by Sabri Çetin on 21.07.2024.
+//
+
+import XCTest
+
+final class Angry_Birds_CloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
